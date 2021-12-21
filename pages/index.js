2cd26444ef882a78f99ddf2e4b4017de909406d3
Anthony Austin-Walker <anthonyaustinwalker@Anthonys-MacBook-Air.local>
2021-12-21 10:42:15 -0600

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Facebook</title>
+      </Head>
+      {/* HEADER */}
+      <Header/>
+      <main>
+        {/* SIDEBAR */}
+        {/* FEED */}
+        {/* WIDGETS */}
+      </main>
+    </div>
+  )
+}
